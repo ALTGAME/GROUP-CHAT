7e94d0afc72e8470f6424e7419f51dd54fa66ce9
ALTGAME <krgpsh13@gmail.com>
2023-09-07 14:36:35 +0530

    signup functionality

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,20 @@ const dotenv = require('dotenv');
 const express = require('express');
 const{chats}= require("./data/data");
 const connectDB = require('./config/db');
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json()); // to accept json data
+
 app.get('/', (req,res)=>{
     res.send("API is running");
 
-})
+});
 
-app.get("/api/chat", (req,res)=>{
-    res.send(chats);
-})
-
-app.get("/api/chat/:id", (req,res)=>{
-   //console.log(req.params.id);
-   const singleChat = chats.find(c=>c._id === req.params.id);
-   res.send(singleChat);
-})
+app.use('/api/user', userRoutes)
 
 
 const PORT = process.env.PORT || 5000
